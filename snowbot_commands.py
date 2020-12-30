53c2b407d693c6f2c5d22c35f03ce213d346f892
kamenrider-yu <69544415+kamenrider-yu@users.noreply.github.com>
2020-12-31 12:23:47 +1300

    Add files via upload

---
+++ snowbot_commands.py
@@ -0,0 +1,19 @@
+command_list = {
+    'hello' : 'hello!',
+    'gm' : 'gm!',
+    'gn' : 'good night😴'
+}
+
+command_question = ['hello' , 'gm' , 'gn']
+
+random_reply_list = [
+'☃️do you want to build a snowman?☃️',
+'very chilly',
+'❄️xue hua piao piao❄️', 
+'hello world!',
+'mmm yum',
+';-;',
+'eheu!',
+'hello there!',
+
+] 
