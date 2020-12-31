82be6efb27234b1399e6af66dbbbe1e1011ae9fb
kamenrider-yu <69544415+kamenrider-yu@users.noreply.github.com>
2020-12-31 16:27:14 +1300

    Update snowbot.py

--- snowbot.py
+++ snowbot.py
@@ -2,9 +2,9 @@ import discord
 import random
 import snowbot_commands
 
-TOKEN = "TOKEN HERE"
+TOKEN = "Your TOKEN here"
 client = discord.Client()
-
+hornyteen_id = 777795314706939915
 
 autosend_delmsg = False
 
@@ -28,39 +28,39 @@ async def on_message(message):
         return
 
 
-    if str(message.content).lower() in snowbot_commands.command_question:
+    if (message.content).lower() in snowbot_commands.command_list :
         await message.channel.send(snowbot_commands.command_list[str(message.content).lower()])
 
     if 'snowbot' == str(message.content).lower():
         randomessage = random.choice(snowbot_commands.random_reply_list)
         await message.channel.send(randomessage)
     
-    if 'snowbot ' in str(message.content).lower():
-        if 'on' in str(message.content).lower():
+    if 'snowbot allsnipe' in str(message.content).lower():
+        if 'on' in str(message.content).lower() and message.author.id in snowbot_commands.authorised:
             autosend_delmsg = True
             await message.channel.send('turned on')
-        elif 'off' in str(message.content).lower():
+        elif 'off' in str(message.content).lower() and message.author.id in snowbot_commands.authorised:
             autosend_delmsg = False
             await message.channel.send('turned off')
-        elif message.author != 454755817179054081:
-            await message.channel.send('you cant do that noob')
         else:
-            await message.channel.send('missing an argument')
+            await message.channel.send("you can't do that Noob")
 
-
+ 
 
 
 
 @client.event
 async def on_message_delete(message_del):
     global autosend_delmsg
-    deleted_message = (f'{message_del.content} {message_del.author} {message_del.channel} {message_del.guild}')
+    global deleted_message
+    deleted_message = (f'{message_del.content} {message_del.author} {message_del.channel} {message_del.guild} ')
     prGreen(deleted_message)
     if autosend_delmsg == True:
-        if message_del.channel == 777795314706939915:
+        if message_del.channel == 777795314706939915 or '-sniped by snowbot' in message_del.content:
             return
         else:
-            await message_del.channel.send(deleted_message)
+            await message_del.channel.send('`' + deleted_message + ' -sniped by snowbot`')
 
-
+#@client.event
+#async def on_message_edit()#
 client.run(TOKEN,bot=False)
